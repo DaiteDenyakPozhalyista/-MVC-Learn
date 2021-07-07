3c5a454aaab3cb545f0a64311a60c5b391f7927a
DaiteDenyakPozhalyista <86009630+DaiteDenyakPozhalyista@users.noreply.github.com>
2021-07-07 14:38:10 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using BookingAppStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingAppStore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        BookContext db = new BookContext();
+        public ActionResult Index()
+        {
+            var books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Спасибо за покупку, " + purchase.Person;
+        }
+
+        public  ActionResult Sell()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Sell(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
